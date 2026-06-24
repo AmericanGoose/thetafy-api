d6d7333396ab509ff3ea2067fd0ef3340712dc8f
AmericanGoose <66270537+AmericanGoose@users.noreply.github.com>
2026-06-24 13:52:48 -0700

    Update funcs.py
    Implemented yt-dlp engine

--- funcs.py
+++ funcs.py
@@ -1,47 +1,29 @@
-from requests import get
-from bs4 import BeautifulSoup
+import yt_dlp
 from typing import List, Union
-
-
-def get_id(url):
-    main_url = f"https://api.fabdl.com/spotify/get?url={url}"
-    res = get(main_url)
-    if res.status_code != 200:
-        return None
-    else:
-        js = res.json()
-        if js is not None:
-            track_id = js['result']['id']
-            return [js['result']['gid'], track_id]
-
-
-def get_down_link(id,track_id):
-    main_url = f"https://api.fabdl.com/spotify/mp3-convert-task/{id}/{track_id}"
-    res = get(main_url)
-    if res.status_code != 200:
-        return None
-    else:
-        js = res.json()
-        if js is not None:
-            return f"https://api.fabdl.com{js['result']['download_url']}"
-        
-
-
 
 def DownloadMusic(UrlList: Union[List[str], str]) -> List[str]:
     audio_urls = []
 
-    if type(UrlList)!=list:
+    if type(UrlList) != list:
         return "required list"
 
-    for i in UrlList:
-        id_for_download = get_id(i)
-        if id_for_download is not None:
-            gid,track_id = id_for_download[0], id_for_download[1]
-            download_link = get_down_link(gid,track_id)
-            if download_link is not None:
-                audio_urls.append(download_link)
+    # Settings for the downloader to only grab audio
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'noplaylist': True,
+        'quiet': True,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        for i in UrlList:
+            try:
+                # We use the Spotify URL/ID to search YouTube for the exact audio track
+                info = ydl.extract_info(f"ytsearch1:{i} audio", download=False)
+                if 'entries' in info and len(info['entries']) > 0:
+                    # Grab the direct audio stream link
+                    url = info['entries'][0]['url']
+                    audio_urls.append(url)
+            except Exception as e:
+                print(f"Failed to scrape: {e}")
+                
     return audio_urls
-
-
-
