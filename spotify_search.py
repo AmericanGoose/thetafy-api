5ab551e514fdecd918d1da06883cb04eb16d6cd7
AmericanGoose <66270537+AmericanGoose@users.noreply.github.com>
2026-06-24 14:08:45 -0700

    Update spotify_search.py
    Updated to allow search by Song Name && Artist Name

--- spotify_search.py
+++ spotify_search.py
@@ -4,12 +4,11 @@ from spotipy.oauth2 import SpotifyClientCredentials
 client_id = 'a25f4d59e37d49029f5113908d583c27'
 client_secret = '617fc5c8f0634a26a34bcfc956a25921'
 
-# If you have any questions about client_id and client_secret, welcome to ask Telegram: @ai_junior
-
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 def SearchFromSpotify(track_name,limit):
     results = sp.search(q=track_name, type='track', limit=limit)
-    track_urls = [item["external_urls"]["spotify"] for item in results["tracks"]["items"]]
-    return track_urls
+    # THE FIX: This combines the Song Name and the Artist Name into a search phrase
+    track_queries = [f"{item['name']} {item['artists'][0]['name']}" for item in results["tracks"]["items"]]
+    return track_queries
